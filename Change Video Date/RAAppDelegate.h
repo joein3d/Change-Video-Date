8f62396e20ad791b500af565e9ff990e1789e55a
joein3d <joein3d@gmail.com>
2013-04-03 07:47:16 -0400

    Initial Commit

---
+++ Change Video Date/RAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RAAppDelegate.h
+//  Change Video Date
+//
+//  Created by Joe on 4/3/13.
+//  Copyright (c) 2013 relaxedapps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Change Video Date/RAAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  RAAppDelegate.m
+//  Change Video Date
+//
+//  Created by Joe on 4/3/13.
+//  Copyright (c) 2013 relaxedapps. All rights reserved.
+//
+
+#import "RAAppDelegate.h"
+
+@implementation RAAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
